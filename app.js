f5983b996bc11d7761b55087015706df49a86310
Jonathan <jagm2100@gmail.com>
2021-06-23 02:04:01 -0500

    prueba2

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path=require('path')
 const publicPath = path.resolve(__dirname,'./public');
 app.use (express.static(publicPath));
 
-app.listen(process.env.PORT || 3000,()=>console.log('Servidor funcionando en el puerto 3000'))
-
+app.listen(process.env.PORT || 3000,function(){
+    console.log('Servidor funcionando en el puerto 3000')})
 
 
 app.get('/',(req,res)=>{
